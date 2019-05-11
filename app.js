94d45501bb80467bbadd8e6c63bf1666ab114eb4
Sitthinon Chanaritthichai <sitthinon.c@hotmail.com>
2019-05-11 16:38:24 +0700

    install swagger

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+var argv = require('minimist')(process.argv.slice(2));
+var swagger = require( 'swagger-node-express');
+var bodyParser = require( 'body-parser' );
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -8,6 +12,63 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+var swaggerJSDoc = require('swagger-jsdoc');
+
+var subpath = express();
+
+app.use(bodyParser());
+app.use("/v1", subpath);
+swagger.setAppHandler(subpath);
+
+app.use(express.static('dist'));
+swagger.setApiInfo({
+  title: "example API",
+  description: "API to do something, manage something...",
+  termsOfServiceUrl: "",
+  contact: "yourname@something.com",
+  license: "",
+  licenseUrl: ""
+});
+
+subpath.get('/', function (req, res) {
+  res.sendFile(__dirname + '/dist/index.html');
+});
+
+swagger.configureSwaggerPaths('', 'dist/api-docs.json', '');
+
+var domain = 'localhost';
+if(argv.domain !== undefined)
+    domain = argv.domain;
+else
+    console.log('No --domain=xxx specified, taking default hostname "localhost".');
+var applicationUrl = 'http://' + domain;
+swagger.configure(applicationUrl, '1.0.0');
+
+// swagger definition
+var swaggerDefinition = {
+  info: {
+    title: 'Node Swagger API',
+    version: '1.0.0',
+    description: 'Demonstrating how to describe a RESTful API with Swagger',
+  },
+  host: domain + ':' + normalizePort(process.env.PORT || '3000'),
+  basePath: '/',
+};
+// options for the swagger docs
+var options = {
+  // import swaggerDefinitions
+  swaggerDefinition: swaggerDefinition,
+  // path to the API docs
+  apis: ['./**/routes/*.js','routes.js'],// pass all in array 
+  };
+// initialize swagger-jsdoc
+var swaggerSpec = swaggerJSDoc(options);
+
+// serve swagger 
+app.get('/swagger.json', function(req, res) {   
+  res.setHeader('Content-Type', 'application/json');   
+  res.send(swaggerSpec); 
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -37,5 +98,22 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
 
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,49 @@
 var express = require('express');
 var router = express.Router();
 
+/**
+ * @swagger
+ * definition:
+ *   users:
+ *     properties:
+ *       name:
+ *         type: string
+ *       email:
+ *         type: string
+ *       age:
+ *         type: integer
+ *       sex:
+ *         type: string
+ */
+
+/**
+ * @swagger
+ * /users:
+ *   get:
+ *     tags:
+ *       - users
+ *     description: Returns all users
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: An array of users
+ *         schema:
+ *           $ref: '#/definitions/users'
+ */
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send({
+    "name":"222",
+    "email":"222",
+    "age":"10",
+    "sex":"1s",
+  });
+});
+
+router.get('/hi', function(req, res, next) {
+  res.send('Hi');
 });
 
 module.exports = router;
